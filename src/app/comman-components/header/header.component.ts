8fe2d6d746b6408cd70455a6bb035fd5ffbc2af9
saba <sabaqaiser39@gmail.com>
2020-10-06 19:33:46 +0530

    updated code

--- src/app/comman-components/header/header.component.ts
+++ src/app/comman-components/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewEncapsulation} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   visibleSidebar1: boolean = false;
+  // closeResult: string;
 
   constructor(private router: Router) { }
+
+  // openVerticallyCentered(content) {
+  //   this.modalService.open(content, { centered: true });
+  // }
 
   ngOnInit(): void {
   }
